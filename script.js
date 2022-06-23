d53cbc41c4d1787429d00d76d0a5249bd99b8d5e
ndchinh <ndc29112001@gmail.com>
2022-06-23 16:57:31 +0700

    sửa thư mục

--- script.js
+++
@@ -1,9 +0,0 @@
-function myFunction1(id="") {
-    document.querySelectorAll('.demo').forEach(element => {
-        element.style.display='none'
-        
-    });
-   document.getElementById(id).style.display='block'
-  
-  }
- console.log(document.querySelectorAll('.demo'))
